39c91b6a617ced5e65e7cc722b29d95fde274001
erazocar <63679019+erazocar@users.noreply.github.com>
2020-05-15 01:42:24 +0200

    second commit

--- data.js
+++ data.js
@@ -7,6 +7,35 @@ export default class data {
 *@param {string} [location]
 */
 
+/**
+ * 
+ */
+output = function() {
+    return this.data;
+}
+
+location = function() {
+    var cityname = "";
+    var base_url = "";
+    var units= extend ;     
+}
+
+getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET',url,true);
+    xhr.responseType = 'json';
+    xhr.onload = function(){
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status,xhr.response);
+        }
+    };
+    xhr.send();    
+};
+
+
 getCurrentWeather(location) {
     return "Weather is sunny for "+location+"based on the data of "+this.source;
 }
