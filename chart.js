f49345c6dc03f8a360e9310b05f3c3d4bce1b9dc
erazocar <63679019+erazocar@users.noreply.github.com>
2020-05-15 01:42:50 +0200

    first commit

---
+++ chart.js
@@ -0,0 +1,29 @@
+export default class chart {
+gimage = require('google-image-chart').charts;
+    constructor() {
+        this.data= data;
+    }
+/**
+*@param {array} [saved]
+*/
+chart = function (arr) {
+    options = {
+        main: false,
+        width: 900,
+        height: 250,
+        bands: [],
+        lines: [],
+        points: [],
+    }
+
+    var chart = new gimage.lines({
+        width: options.width,
+        height: options.height,
+        bands: options.bands,
+        hlines: options.lines,
+        points: options.points,
+        autoscale: true
+    });
+    return chart.render();
+}
+}
