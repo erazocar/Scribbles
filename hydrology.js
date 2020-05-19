ae38516cc5cd5d197f3e76c5e7e13faa1b789b9b
erazocar <63679019+erazocar@users.noreply.github.com>
2020-05-19 23:32:02 +0200

    fourth commit

---
+++ hydrology.js
@@ -0,0 +1,39 @@
+export default class hydro {
+    constructor() {
+        this.source= source
+        this.data = data;
+    }
+
+    /**Arithmetic mean: computation of aereal mean precipitation for a river basin given it has different stations.
+     * @param {data} object with time series data for different stations throughout the basin.
+     * @returns {array} object with average precipitaiton for a specific time series.
+     */
+    arithmetic = function(data) {
+        var arr1 = [];
+        for (var i=0; i < arr.length; i++){
+           arr1.push()             
+        }
+     }
+     /**Thiessen polygon: calculates average precipitation for a basin considering there is
+      * one station per sub basin.
+      * @param {data} object with arrays of time series.
+      * @param {data} object with arrays of areas per subbasin.
+      * @returns {array} time series of average precipitation over whole sub basin.
+      */
+     thiessen = function(arr1, arr2) {
+        var count = arr2.count();
+        outputarr = [];
+        for (var i = 0; i<arr1.length; i++){
+            outputarr[i]= arr1[i]*arr2[i]
+        }
+    }
+
+    /** rainfallrunoff: does simple rainfall-runoff analyses over a rainfall dataset given landuse, baseflow and infiltration capacity.
+     * @param {array} dataset with rainfall information.
+     * @param {param} parameter file from JSON object for landuse and infiltration capacity.
+     * @param {var} var with baseflow information. 
+     */
+    rainfallrunoff = function(arr, parm, vars){
+
+    }
+}
--- stats.js
+++ stats.js
@@ -1,153 +1,189 @@
 export default class stats {
 
     constructor() {
-        this.source= source;
+        this.data= data;
     }
-//var isArray = Array.isArray || function isArray(arg) {
-//return toString.call(arg) === '[Object Array]'
-//
 
-/**
-*@param {arr} [retrieved data]
-*@return {Number, Array}
-*/
-
-/**
- * output of data
- */
-output = function(arr){
-    return this.data;
-}
-
-/**
- * calculate the sum of the values of an array
- */
- sum = function (arr) {
-    var sum = 0;
-    var i = arr.length;
-    while (--i >=0)
-        sum += arr[i];
-    return sum;
-};
-
-/**
-*calculate statistic mean of an array
-*/
-mean = function(arr) {
-    var total = 0;
-    total = this.sum(arr)
-    m = total / arr.length;
-    return m;
-};
-
-/** 
- * calculate the standard deviation of an array
- */
-stddev = function (arr) {
-    var mean = this.mean();
-    var SD = 0;
-    var nex =[];
-    for(var i=0; i<arr.length;i+=1){
-        nex.push((arr[i]-mean)*(arr[i]-mean));
-    }
-    return SD=Math.sqrt(Math.sum(nex)/nex.length);
-};
-
-/**
- * calculate the squared sum of the values of an array
- */
-sumsqrd = function(arr) {
-    var sum = 0;
-    var i = arr.length;
-    while (--i >=0)
-        sum+= arr[i];
-    return sum;
-};
-
-/**
- * minimum value of an array
- */
-min = function(arr) {
-    var low = arr[0];
-    var i=0;
-    while (++i <arr.length)
-        if (arr[i] <low)
-            low = arr[i];
-    return low;
-};
-
-/**
- * maximum value of an array
- */
-max = function(arr) {
-    var high=arr[0];
-    var i=0;
-    while (++i < arr.length)
-        if (arr[i] > high)
-            high = arr[i];
-    return high
-};
-
-/**
- * unique values in an array
- */
-unique = function(arr) {
-    var un = {}, _arr = [];
-    for(var i=0; i<arr.length; i++){
-        if (!un[arr[i]]){
-            un[arr[i]] = true;
-            _arr.push(arr[i]);
+    /** copydata: makes a copy of original data.
+     * @param {data} original data.
+     * @returns {data} copy of original data.
+     */
+    copydata = function(data){
+        var arr = [];
+        for (i=0; i<data.length;i++){
+            arr.push(data[i]);
         }
+        return arr;
     }
-    return _arr;
-};
 
-/**
- * standardize data
- */
-standardize = function(arr) {
-    var _arr = [];
-    var stddev = this.stddev();
-    var mean = this.mean();
-    for (var i=0; i<arr.length; i++) {
-        _arr[i]=(arr[i]-mean(arr))/stddev(arr);
+    /** onearray: converts data object into 1d array.
+     * @param {data} data object, either original or copy.
+     * @returns {array}
+     */
+    onearray = function(data){
+        var arr = [];
+        for (var i=0; data.length;i++){
+            arr.push(data[0][i]);
+        }
+        return arr;
     }
-    return _arr;
-};
 
-/**
- * quantile calculator for the given data, q in number (ie. 25, 75)
- * adapted from simple statistics with php
- */
-quantile = function(arr,q){
-    var _arr=arr.slice();
-    _arr.sort(function(a,b){
-        return a-b
-    });
-    var p = (arr.length - 1) * q;
-    var b = Math.floor(p);
-    var rest = p - b;
-    if ((_arr[b+1]!==undefined)){
-        return _arr[b] + rest * (_arr[b+1] - _arr[b]);
-    } else {
-        return _arr[b];
-    }   
-};
+    /** sum: sums all data in a series.
+     * @param {data} data either original or copy from original.
+     * @returns {var} sum of all data in an array.
+     */
+    sum = function (data) {
+        var arr = dataarray(data);
+        var sum = 0;
+        var i = arr.length;
+        while (--i >=0)
+            sum += arr[i];
+        return sum;
+    };
 
-/**
- * obtain outliers in dataset
- */
-outliers = function(arr) {
-    var Q_25 = this.quantile(arr,0.25);
-    var Q_75 = this.quantile(arr,0.75);
-    var IQR = Q_75-Q_25;
-    var out = [];
-    for (var i=0; i <arr.length;i++){
-        if (arr[i] < (1.5*IQR-Q_25) || arr[i] > (1.5*IQR+Q_75)) {
-            out.push(arr[i]);
-        } 
+    /** mean: calculates the mean of a 1d array.
+     * @param {array} array with data.
+     * @returns {var} mean of the data.
+     */
+    mean = function(data) {
+        var arr = dataarray(data);
+        var total = 0;
+        total = this.sum(arr)
+        m = total / arr.length;
+        return m;
+    };
+
+    /** stdev = calculates standard deviation of an array.
+     * @param {array} array with data.
+     * @returns {var} variable with standard deviation.
+     */
+    stddev = function (arr) {
+        var mean = this.mean();
+        var SD = 0;
+        var nex =[];
+        for(var i=0; i<arr.length;i+=1){
+            nex.push((arr[i]-mean)*(arr[i]-mean));
+        }
+        return SD=Math.sqrt(Math.sum(nex)/nex.length);
+    };
+
+    /** sumsqrd: calculates sum of squares for a dataset.
+     * @param {array} array with data.
+     * @returns {var} variable with sum of squares for data.
+     */
+    sumsqrd = function(arr) {
+        var sum = 0;
+        var i = arr.length;
+        while (--i >=0)
+            sum+= arr[i];
+        return sum;
+    };
+
+    /** min: minimum value of an array
+     * @param {array} array with data.
+     * @returns {var} variable with min value of dataset.
+     */
+    min = function(arr) {
+        var low = arr[0];
+        var i=0;
+        while (++i <arr.length)
+            if (arr[i] <low)
+                low = arr[i];
+        return low;
+    };
+
+    /** max: maximum value of an array.
+     * @param {array} array with data.
+     * @returns {var} variable with max value of dataset.
+     */
+    max = function(arr) {
+        var high=arr[0];
+        var i=0;
+        while (++i < arr.length)
+            if (arr[i] > high)
+                high = arr[i];
+        return high
+    };
+
+    /** unique: unique values in an array.
+     * @param {array} array with data.
+     * @returns {array} array with unique values.
+     */
+    unique = function(arr) {
+        var un = {}, _arr = [];
+        for(var i=0; i<arr.length; i++){
+            if (!un[arr[i]]){
+                un[arr[i]] = true;
+                _arr.push(arr[i]);
+            }
+        }
+        return _arr;
+    };
+
+    /** standardize: use mean and standard deviation to standardize the original dataset.
+     * @param {array} array with data.
+     * @returns {array} array with standardized data.
+     */
+    standardize = function(arr) {
+        var _arr = [];
+        var stddev = this.stddev();
+        var mean = this.mean();
+        for (var i=0; i<arr.length; i++) {
+            _arr[i]=(arr[i]-mean(arr))/stddev(arr);
+        }
+        return _arr;
+    };
+
+    /** quantile: quantile calculator for given data and q written as (ie. 25, 75)
+     * @param {array} array with data.
+     * @param {var} number of quantile required.
+     * @returns {array} array with values fitting the quartile.
+     */
+    quantile = function(arr,q){
+        var _arr=arr.slice();
+        _arr.sort(function(a,b){
+            return a-b
+        });
+        var p = (arr.length - 1) * q;
+        var b = Math.floor(p);
+        var rest = p - b;
+        if ((_arr[b+1]!==undefined)){
+            return _arr[b] + rest * (_arr[b+1] - _arr[b]);
+        } else {
+            return _arr[b];
+        }   
+    };
+
+    /** outliers: obtain outliers from dataset.
+     * @param {array} array with data.
+     * @returns {array} array with outlier data.
+     */
+    outliers = function(arr) {
+        var Q_25 = this.quantile(arr,0.25);
+        var Q_75 = this.quantile(arr,0.75);
+        var IQR = Q_75-Q_25;
+        var out = [];
+        for (var i=0; i <arr.length;i++){
+            if (arr[i] < (1.5*IQR-Q_25) || arr[i] > (1.5*IQR+Q_75)) {
+                out.push(arr[i]);
+            } 
+        }
+        return out;
+    };
+
+    /** outremove: remove outliers from dataset.
+     * @param {array} array with data.
+     * @returns {array} array with cleaned data.
+     */
+    outremove = function(arr) {
+        var or = arr.slice();
+        var out = this.outliers(arr);
+        for (var i=0; i<or.length;i++){
+            if (or[i]===out[i]){
+                or.splice(i,1);
+                i--;
+            }
+        }
+        return out;
     }
-    return out;
-};
-
 }
